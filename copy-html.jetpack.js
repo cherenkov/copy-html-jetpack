00874ccff5ebe86e1e701cf498140febcc44a617
Erik Vold <erikvvold@gmail.com>
2009-11-30 04:19:48 -0800

    add jetpack

---
+++ copy-html.jetpack.js
@@ -0,0 +1,9 @@
+jetpack.future.import("menu");
+jetpack.future.import("clipboard");
+jetpack.future.import("selection");
+jetpack.menu.context.page.insertBefore( {
+	label: "Copy HTML",
+	command: function(){
+		jetpack.clipboard.set( jetpack.selection.html );
+	}
+}, "Select All" );
